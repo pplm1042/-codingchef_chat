d69a4dcd180dfff7fa0e636cc18d7279cc8ab55f
pplm1042 <pplm1042@gmail.com>
2022-05-09 17:47:03 +0900

    [Update] 코딩셰프 조금 매운맛 강좌 27

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,3 +1,5 @@
+import 'package:codingchef_chat/chatting/chat/message.dart';
+import 'package:codingchef_chat/chatting/chat/new_message.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -45,30 +47,13 @@ class _ChatScreenState extends State<ChatScreen> {
                 icon: Icon(Icons.exit_to_app_sharp, color: Colors.white))
           ],
         ),
-        body: StreamBuilder(
-          stream: FirebaseFirestore.instance
-              .collection('/chats/AQk6LzDrDLN600Y7trw5/message')
-              .snapshots(),
-          builder: (BuildContext context,
-              AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-            final docs = snapshot.data!.docs;
-            return ListView.builder(
-                itemCount: docs.length,
-                itemBuilder: (context, index) {
-                  return Container(
-                    padding: EdgeInsets.all(8.0),
-                    child: Text(
-                      docs[index]['text'],
-                      style: TextStyle(fontSize: 20),
-                    ),
-                  );
-                });
-          },
+        body: Container(
+          child: Column(
+            children: [
+              Expanded(child: Messages()),
+              NewMessage(),
+            ],
+          ),
         ));
   }
 }
