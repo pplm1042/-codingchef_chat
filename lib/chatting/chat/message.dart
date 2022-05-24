d67fbeb926e7b931d9d41251819ac27211e402a8
pplm1042 <pplm1042@gmail.com>
2022-05-24 20:11:14 +0900

    [Update] [Update] 코딩셰프 조금 매운맛 강좌 28

--- lib/chatting/chat/message.dart
+++ lib/chatting/chat/message.dart
@@ -1,11 +1,14 @@
+import 'package:codingchef_chat/chatting/chat/chat_bubble.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class Messages extends StatelessWidget {
   const Messages({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final user = FirebaseAuth.instance.currentUser;
     return StreamBuilder(
       stream: FirebaseFirestore.instance
           .collection('chat')
@@ -25,7 +28,8 @@ class Messages extends StatelessWidget {
             reverse: true,
             itemCount: snapshot.data!.docs.length,
             itemBuilder: (context, index) {
-              return Text(chatDocs[index]['text']);
+              return ChatBubble(chatDocs[index]['text'],
+                  chatDocs[index]['userID'].toString() == user!.uid);
             });
       },
     );
